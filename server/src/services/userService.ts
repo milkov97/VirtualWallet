382f0e41ebd40b0cfb81b11ad5fe168a70b8a6ed
milkov97 <129657588+milkov97@users.noreply.github.com>
2023-09-02 16:50:49 +0300

    add hashed password to database when create user

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -1,6 +1,9 @@
 import User from "../models/user/User";
 import { UserInterface } from "../interfaces/user/UserInterface";
 import { connectToDatabase } from "../database/dbConnection";
+import { hashPassword } from "../utils/security/hashPassword";
+// import { comparePasswords } from "../utils/security/comparePassword";
+
 
 class UserService {
   public async getUser() {
@@ -17,10 +20,11 @@ class UserService {
   public async createUser(userData: UserInterface): Promise<UserInterface> {
     try {
       const db = await connectToDatabase();
+      const hashedPassword = await hashPassword(userData.password)
       const newUser = new User(
         userData.username,
         userData.email,
-        userData.password,  
+        hashedPassword,  
         userData.firstName,
         userData.lastName,
         userData.country,
