f71f840753b21b4f51520d3382cca2bec3898620
milkov97 <129657588+milkov97@users.noreply.github.com>
2023-09-04 19:40:21 +0300

    add cookie

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -4,20 +4,30 @@ import { token } from "../utils/auth/jwtToken";
 
 
 class UserController {
+
   public async login(req: Request, res: Response): Promise<Response> {
     try {
       const userData = req.body
+
       const user = userService.authenticateUser(userData)
+
       if(!user) {
         return res.json({message: 'User not found'}).status(401)
       }
       // @ts-ignore
       const accessToken = token.createToken({id: user.id, username: user.username, email: user.email})
-      return res.json(token.verifyToken(accessToken)?.payload).status(200)
+
+      res.cookie("accessToken", accessToken, {
+        maxAge: 300000,
+        httpOnly:true,
+      })
+      
+      return res.json(token.verifyToken(accessToken)?.payload)
     } catch (error) {
       return res.json({ error: "Internal server error" }).status(500);
     }
   }
+
   public async createUser(req: Request, res: Response): Promise<Response> {
     try {
       const userData = req.body;
