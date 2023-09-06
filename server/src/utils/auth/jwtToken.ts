41260b175de393abb6bf761e8b8cafa51417d270
milkov97 <129657588+milkov97@users.noreply.github.com>
2023-09-06 10:41:15 +0300

    add refreshToken

--- server/src/utils/auth/jwtToken.ts
+++ server/src/utils/auth/jwtToken.ts
@@ -1,12 +1,12 @@
 import jwt, { Secret, Algorithm } from "jsonwebtoken";
 
 class Token {
-  public createToken(payload: object) {
-    
+  public createToken(payload: object, privateKey?: Secret, expiresIn?: string) {
     try {
       // @ts-ignore
       const hashAlgorithm: Algorithm = process.env["JWT_ALGORITHM"];
-      const privateKey: Secret = process.env["JWT_PRIVATE_KEY"]!;
+      privateKey = process.env["JWT_ACCESS_TOKEN"]!;
+      expiresIn = "10m"
 
       if (!privateKey || !hashAlgorithm) {
         throw new Error("JWT configuration missing");
@@ -14,7 +14,7 @@ class Token {
 
       const token = jwt.sign(payload, privateKey, {
         algorithm: hashAlgorithm,
-        expiresIn: "1h",
+        expiresIn: expiresIn,
       });
       return token;
     } catch (err) {
@@ -22,14 +22,23 @@ class Token {
     }
   }
 
+  public createRefreshToken(payload: object) {
+    try {
+      const privateKey: Secret = process.env["JWT_REFRESH_TOKEN"]!;
+      return this.createToken(payload, privateKey, '1h');
+    } catch (err) {
+      throw new Error(`Failed to create JWT: ${err}`);
+    }
+  }
+
   public verifyToken(token: string) {
     try {
-      const publicKey: Secret = process.env["JWT_PRIVATE_KEY"]!;
+      const publicKey: Secret = process.env["JWT_ACCESS_TOKEN"]!;
       if (!publicKey) {
         throw new Error("JWT configuration missing");
       }
       const decoded = jwt.verify(token, publicKey);
-      
+
       return { payload: decoded, expired: false };
     } catch (error) {
       if (error instanceof Error) {
