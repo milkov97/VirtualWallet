9eb903b1b988f7606a24556e8223bbb8e5c46be6
milkov97 <129657588+milkov97@users.noreply.github.com>
2023-09-06 10:42:58 +0300

    update response status codes

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,18 +1,19 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { userService } from "../services/userService";
 import { token } from "../utils/auth/jwtToken";
 
 class UserController {
-  public async login(req: Request, res: Response): Promise<Response> {
+  public async login(req: Request, res: Response, next: NextFunction): Promise<Response> {
     try {
       const userData = req.body;
       const user = await userService.authenticateUser(userData);
       if (!user) {
-        return res.json().status(500);
+        return res.status(401).json("Incorrect username or password");
       }
       // @ts-ignore
       const payload = { username: user.username };
       const accessToken = token.createToken(payload);
+      const refreshToken = token.createRefreshToken(payload)
 
       res.cookie("accessToken", accessToken, {
         maxAge: 300000,
@@ -24,8 +25,8 @@ class UserController {
       let message = "Unknown Error";
       if (error instanceof Error) message = error.message;
       return res
-        .json({ error: `Internal server error: ${message}` })
-        .status(500);
+        .status(500)
+        .json({ error: `Internal server error: ${message}` });
     }
   }
 
@@ -35,13 +36,13 @@ class UserController {
 
       const newUser = await userService.createUser(userData);
 
-      return res.json(newUser).status(201);
+      return res.status(201).json(newUser);
     } catch (error) {
       let message = "Unknown Error";
       if (error instanceof Error) message = error.message;
       return res
-        .json({ error: `Internal server error: ${message}` })
-        .status(500);
+        .status(500)
+        .json({ error: `Internal server error: ${message}` });
     }
   }
 }
