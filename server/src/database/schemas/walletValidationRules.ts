e79361312b6feb03608e47ee5019e3a809c9ad89
milkov97 <129657588+milkov97@users.noreply.github.com>
2023-09-07 17:23:22 +0300

    add wallet validation schema

---
+++ server/src/database/schemas/walletValidationRules.ts
@@ -0,0 +1,23 @@
+export const walletValidationRules = {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            required: [
+                "_id",
+                "balance",
+                "currency"
+            ],
+            properties: {
+                _id: {
+                    bsonType: "objectId",
+                },
+                balance: {
+                    bsonType: "number",
+                },
+                currency: {
+                    bsonType: "string"
+                }
+            }
+        }
+    }
+}
