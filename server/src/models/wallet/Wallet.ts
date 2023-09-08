fe246334d936b09b0fcac7f6106d99322bba61e9
milkov97 <129657588+milkov97@users.noreply.github.com>
2023-09-08 15:43:36 +0300

    add wallet class

---
+++ server/src/models/wallet/Wallet.ts
@@ -0,0 +1,9 @@
+import { ObjectId } from "mongodb";
+import { CardInterface } from "../card/interfaces/CardInterface";
+import { Currency } from "./enums/Currency";
+import { WalletInterface } from "./interfaces/WalletInterface";
+
+export class Wallet implements WalletInterface {
+    constructor(public ownerId: ObjectId, public balance: number, public currency: Currency, public cards: CardInterface[]) {}
+}
+
--- server/src/models/wallet/interfaces/WalletInterface.ts
+++ server/src/models/wallet/interfaces/WalletInterface.ts
@@ -1,9 +1,11 @@
 import { ObjectId } from "mongodb";
 import { Currency } from "../enums/Currency";
+import { CardInterface } from "../../card/interfaces/CardInterface";
 
 export interface WalletInterface {
-    id: ObjectId;
+    _id?: ObjectId;
     ownerId: ObjectId;
     balance: number;
-    currency: Currency.BGN | Currency.EUR | Currency.USD
+    currency: Currency.BGN | Currency.EUR | Currency.USD;
+    cards: CardInterface[];
 }
