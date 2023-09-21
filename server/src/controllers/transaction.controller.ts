83c11d5e3bbfc17ed8449f13e3db5754c2529d9e
milkov97 <129657588+milkov97@users.noreply.github.com>
2023-09-21 20:40:08 +0300

    add base functions

--- server/src/controllers/transaction.controller.ts
+++ server/src/controllers/transaction.controller.ts
@@ -3,7 +3,30 @@ import { transactionService } from "./../services/transaction.services";
 
 
 class TransactionController{
+  public async transferMoneyToWallet(req: Request, res: Response) {
+    
+  }
 
+  public async sendMoney(req: Request, res: Response) {
+    
+  }
+  
+  public async confirmTransaction(req: Request, res: Response) {
+
+  }
+
+  public async viewTransactionHistory(req: Request, res: Response) {
+
+  }
+
+  public async sendTransactionNotification(req: Request, res: Response) {
+
+  }
+
+  public async exchangeMoney(req: Request, res: Response) {
+
+  }
+  
 }
 
 export const transactionController = new TransactionController()
